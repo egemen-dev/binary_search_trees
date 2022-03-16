cb996a50c5a63bd86a4c9b5e6c0147e4146e044a
Egemen <egemenwasd@gmail.com>
2022-03-16 19:20:38 +0300

    Indentation fixed.

--- lib/node.rb
+++ lib/node.rb
@@ -1,10 +1,9 @@
 class Node
-    attr_accessor :data, :left, :right
-  
-    def initialize(data)
-      @data = data
-      @left = nil
-      @right = nil
-    end
+  attr_accessor :data, :left, :right
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
 end
-  
