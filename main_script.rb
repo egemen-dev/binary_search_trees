d9125089304fbc01c22d08f0745d263e9ba5bc5e
Egemen <egemenwasd@gmail.com>
2022-03-16 19:24:08 +0300

    Indentation fixed and comment deleted.

--- main_script.rb
+++ main_script.rb
@@ -10,19 +10,18 @@ puts "first tree preorder    ->  #{tree_1.preorder}"
 puts "first tree postorder   ->  #{tree_1.postorder}"
 puts "first tree inorder     ->  #{tree_1.inorder}"
 
-puts "\nInsertions to unbalance the tree\n"
+puts "\nInsertions to unbalance the tree\n "
 tree_1.insert(7600)
 tree_1.insert(7650)
 tree_1.insert(7670)
 tree_1.pretty_print
 
 puts "first tree balanced after insertions? ->  #{tree_1.balanced?}"
-puts "new balanced tree created ->  #{tree_2 = tree_1.rebalance}"
-puts "new balanced tree ^^^^^^^^^^^^ #{tree_2.pretty_print}"
-puts "new tree balanced?   ->  #{tree_2.balanced?}"
-puts "new tree level order ->  #{tree_2.level_order}"
-puts "new tree preorder    ->  #{tree_2.preorder}"
-puts "new tree postorder   ->  #{tree_2.postorder}"
-puts "new tree inorder     ->  #{tree_2.inorder}"
-
-# All the other Tree methods works perfectly too, such as #delete, #succesosr, #find, #height...
+puts "first tree balanced after insertions? ->  #{tree_1.balanced?}"
+puts "balances the current tree ->  #{tree_1.rebalance!}"
+puts "balanced tree ^^^^^^^^^^^^ #{tree_1.pretty_print}"
+puts "tree balanced?   ->  #{tree_1.balanced?}"
+puts "tree level order ->  #{tree_1.level_order}"
+puts "tree preorder    ->  #{tree_1.preorder}"
+puts "tree postorder   ->  #{tree_1.postorder}"
+puts "tree inorder     ->  #{tree_1.inorder}"
